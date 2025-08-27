3e4ac38df3e76ed50083955242e7286d6d2c3cad
vladi <vladi@ASPIRE3>
2025-08-27 15:27:04 +0300

    [Add] Добавлена часть тестов на xUnit

---
+++ TextEditor.Tests/TestAppBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Headless;
+using Avalonia;
+using TextEditor.Tests;
+
+[assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+namespace TextEditor.Tests
+{
+    public class TestAppBuilder
+    {
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+            .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+    }
+}
---
+++ TextEditor.Tests/ViewModels/CreationViewModelTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Headless.XUnit;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using TextEditor.ViewModels;
+using TextEditor.Views;
+using Xunit;
+
+namespace TextEditor.Tests
+{
+    public class CreationViewModelTests
+    {
+
+        [AvaloniaFact]
+        public void Save_NullPath_CloseWindow()
+        {
+            // Arrange
+            var creationWindow = new CreationWindow();
+            var creationViewModel = new CreationViewModel();
+            creationWindow.DataContext = creationViewModel;
+            creationWindow.Show();
+
+            creationViewModel.TextFile.Name = "TestFile";
+            creationViewModel.TextFile.Path = null;
+
+            //Act
+            creationViewModel.Save(creationWindow);
+
+            //Assert
+            Assert.True(creationWindow.IsVisible);
+        }
+
+
+        [AvaloniaFact]
+        public void Save_NullName_CloseWindow()
+        {
+            // Arrange
+            var creationWindow = new CreationWindow();
+            var creationViewModel = new CreationViewModel();
+            creationWindow.DataContext = creationViewModel;
+            creationWindow.Show();
+
+            creationViewModel.TextFile.Name = null;
+            creationViewModel.TextFile.Path = "C:\\Users\\vladi\\Downloads";
+
+            //Act
+            creationViewModel.Save(creationWindow);
+
+            //Assert
+            Assert.True(creationWindow.IsVisible);
+        }
+
+        [AvaloniaFact]
+        public void Save_FullData_CloseWindow()
+        {
+            // Arrange
+            var creationWindow = new CreationWindow();
+            var creationViewModel = new CreationViewModel();
+            creationWindow.DataContext = creationViewModel;
+            creationWindow.Show();
+
+            creationViewModel.TextFile.Name = "TextFile";
+            creationViewModel.TextFile.Path = "C:\\Users\\vladi\\Downloads";
+
+            //Act
+            creationViewModel.Save(creationWindow);
+
+            //Assert
+            Assert.False(creationWindow.IsVisible);
+        }
+    }
+}
